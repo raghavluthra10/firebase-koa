d87419513dcce22cda6977024fe2fde2f3051038
raghavluthra10 <77231964+raghavluthra10@users.noreply.github.com>
2022-08-02 19:17:59 +0530

    setup for react done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Koa = require("koa");
 const app = new Koa();
 const Router = require("koa-router");
+const BodyParser = require("koa-bodyparser");
 const appRoutes = require("./routes");
 
 const router = new Router();
 
+app.use(BodyParser());
 app.use(appRoutes.routes());
 
 app.listen(3000);
